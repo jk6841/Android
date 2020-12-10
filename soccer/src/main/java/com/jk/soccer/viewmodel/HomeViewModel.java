251a511721673f1510fa189519b5f11a7a7b5ff4
jk6841 <jk6841@gmail.com>
2020-12-11 01:24:25 +0900

    basic operation

--- soccer/src/main/java/com/jk/soccer/viewmodel/HomeViewModel.java
+++ soccer/src/main/java/com/jk/soccer/viewmodel/HomeViewModel.java
@@ -5,7 +5,10 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.Transformations;
 
 import com.jk.soccer.R;
@@ -14,27 +17,37 @@ import com.jk.soccer.data.local.Player;
 import com.jk.soccer.data.local.Team;
 import com.jk.soccer.ui.home.HomeAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeViewModel extends AndroidViewModel {
 
+    private final Application application;
     private Repository repository;
-
     private HomeAdapter adapter;
 
-    private LiveData<List<Player>> livedataPlayer;
+    private MutableLiveData<Integer> index;
+
+    final private LiveData<List<Player>> livedataPlayers;
     private LiveData<List<Team>> livedataTeam;
 
-    final private LiveData<String> livedataPlayerName;
-    final private LiveData<Boolean> livedataBookmark;
+    final private ArrayList<LiveData<String>> livedataPlayerName;
+    final private ArrayList<LiveData<Boolean>> livedataBookmark;
     //final private LiveData<String> livedataTeamName;
-    final private LiveData<String> livedataPosition;
-    final private LiveData<String> livedataHeight;
-    final private LiveData<String> livedataFoot;
-    final private LiveData<String> livedataAge;
-    final private LiveData<String> livedataShirt;
-    final private LiveData<String> livedataPlayerUrl;
+    final private ArrayList<LiveData<String>> livedataPosition;
+    final private ArrayList<LiveData<String>> livedataHeight;
+    final private ArrayList<LiveData<String>> livedataFoot;
+    final private ArrayList<LiveData<String>> livedataAge;
+    final private ArrayList<LiveData<String>> livedataShirt;
+    final private ArrayList<LiveData<String>> livedataPlayerUrl;
 
+    public MutableLiveData<Integer> getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index.setValue(index);
+    }
 
     public HomeAdapter getAdapter() {
         return adapter;
@@ -44,71 +57,82 @@ public class HomeViewModel extends AndroidViewModel {
         this.adapter = adapter;
     }
 
-    public LiveData<String> getLivePlayerName() {
-        return livedataPlayerName;
+    public LiveData<String> getLivePlayerName(int index) {
+        return livedataPlayerName.get(index);
     }
 
-    public LiveData<String> getLivePosition() {
-        return livedataPosition;
+    public LiveData<String> getLivePosition(int index) {
+        return livedataPosition.get(index);
     }
 
-    public LiveData<String> getLiveHeight() {
-        return livedataHeight;
+    public LiveData<String> getLiveHeight(int index) {
+        return livedataHeight.get(index);
     }
 
-    public LiveData<String> getLiveFoot() {
-        return livedataFoot;
+    public LiveData<String> getLiveFoot(int index) {
+        return livedataFoot.get(index);
     }
 
-    public LiveData<String> getLiveAge() {
-        return livedataAge;
+    public LiveData<String> getLiveAge(int index) {
+        return livedataAge.get(index);
     }
 
-    public LiveData<String> getLiveShirt() {
-        return livedataShirt;
+    public LiveData<String> getLiveShirt(int index) {
+        return livedataShirt.get(index);
     }
 
-    public LiveData<String> getLivePlayerUrl() {
-        return livedataPlayerUrl;
+    public LiveData<String> getLivePlayerUrl(int index) {
+        return livedataPlayerUrl.get(index);
     }
 
-    public LiveData<List<Player>> getLivePlayer(){
-        return livedataPlayer;
+    public LiveData<List<Player>> getLivePlayers(){
+        return livedataPlayers;
     }
 
     public HomeViewModel(@NonNull Application application) {
         super(application);
-
+        this.application = application;
         repository = Repository.getInstance(application);
-        livedataPlayer = repository.getPlayer();
-        livedataTeam = repository.getTeam();
+        livedataPlayers = repository.getPlayer();
+        index = new MutableLiveData<>();
+        livedataPlayerName = new ArrayList<>();
+        livedataPosition = new ArrayList<>();
+        livedataHeight = new ArrayList<>();
+        livedataFoot = new ArrayList<>();
+        livedataAge = new ArrayList<>();
+        livedataShirt = new ArrayList<>();
+        livedataBookmark = new ArrayList<>();
+        livedataPlayerUrl = new ArrayList<>();
 
-        livedataPlayerName = Transformations.map(
-                livedataPlayer, input -> "이름: " + input.get(0).getName() );
+        int len = repository.countPlayer();
 
-        livedataBookmark = Transformations.map(
-                livedataPlayer, input -> input.get(0).isBookmark() );
+        for (int i = 0; i < len; i++){
+            int k = i;
+            livedataPlayerName.add(Transformations.map(
+                    livedataPlayers, input ->  input.get(k).printName() ));
 
-        livedataPosition = Transformations.map(
-                livedataPlayer, input -> "포지션: " + input.get(0).getPosition() );
+            livedataPosition.add(Transformations.map(
+                    livedataPlayers, input -> (input != null) ? input.get(k).printPosition() : null));
 
-        livedataHeight = Transformations.map(
-                livedataPlayer, input -> "신장: " + input.get(0).getHeight() );
+            livedataHeight.add(Transformations.map(
+                    livedataPlayers, input -> (input != null) ? input.get(k).printHeight() : null));
 
-        livedataFoot = Transformations.map(
-                livedataPlayer, input -> "주발: " + input.get(0).getFoot() );
+            livedataFoot.add(Transformations.map(
+                    livedataPlayers, input -> (input != null) ? input.get(k).printFoot() : null));
 
-        livedataAge = Transformations.map(
-                livedataPlayer, input -> "나이: " + input.get(0).getAge() + "세" );
+            livedataAge.add(Transformations.map(
+                    livedataPlayers, input -> (input != null) ? input.get(k).printAge() : null));
 
-        livedataShirt = Transformations.map(
-                livedataPlayer, input -> "등번호: " + input.get(0).getShirt() );
+            livedataShirt.add(Transformations.map(
+                    livedataPlayers, input -> (input != null) ? input.get(k).printShirt() : null));
 
-        livedataPlayerUrl = Transformations.map(
-                livedataPlayer,
-                input -> application.getString(R.string.baseUrl2)
-                        + input.get(0).getId()
-                        + application.getString(R.string.png) );
+            livedataBookmark.add(Transformations.map(
+                    livedataPlayers, input -> (input != null) ? input.get(k).isBookmark() : null));
+
+            livedataPlayerUrl.add(Transformations.map(
+                    livedataPlayers, input -> (input != null) ? application.getString(R.string.baseUrl2)
+                            + input.get(k).getId() + application.getString(R.string.png) : null));
+        }
     }
 
 }
