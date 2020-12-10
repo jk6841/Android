4843ed1b94206c9d7c79e933039396f6dc7234aa
jk6841 <jk6841@gmail.com>
2020-12-10 18:52:23 +0900

    introduce lambda expression

--- soccer/src/main/java/com/jk/soccer/viewmodel/HomeViewModel.java
+++ soccer/src/main/java/com/jk/soccer/viewmodel/HomeViewModel.java
@@ -62,60 +62,37 @@ public class HomeViewModel extends AndroidViewModel {
 
     public HomeViewModel(@NonNull Application application) {
         super(application);
+
         repository = Repository.getInstance(application);
         mlPlayer = repository.getPlayer();
         mlTeam = repository.getTeam();
-        mlPlayerName = Transformations.map(mlPlayer, new Function<List<Player>, String>() {
-            @Override
-            public String apply(List<Player> input) {
-                return "이름: " + input.get(0).getName();
-            }
-        });
-        mlBookmark = Transformations.map(mlPlayer, new Function<List<Player>, Boolean>() {
-            @Override
-            public Boolean apply(List<Player> input) {
-                return input.get(0).isBookmark();
-            }
-        });
-        mlPosition = Transformations.map(mlPlayer, new Function<List<Player>, String>() {
-            @Override
-            public String apply(List<Player> input) {
-                return "포지션: " + input.get(0).getPosition();
-            }
-        });
-        mlHeight = Transformations.map(mlPlayer, new Function<List<Player>, String>() {
-            @Override
-            public String apply(List<Player> input) {
-                return "신장: " + input.get(0).getHeight();
-            }
-        });
-        mlFoot = Transformations.map(mlPlayer, new Function<List<Player>, String>() {
-            @Override
-            public String apply(List<Player> input) {
-                return "주발: " + input.get(0).getFoot();
-            }
-        });
-        mlAge = Transformations.map(mlPlayer, new Function<List<Player>, String>() {
-            @Override
-            public String apply(List<Player> input) {
-                return "나이: " + input.get(0).getAge() + "세";
-            }
-        });
-        mlShirt = Transformations.map(mlPlayer, new Function<List<Player>, String>() {
-            @Override
-            public String apply(List<Player> input) {
-                return "등번호: " + input.get(0).getShirt();
-            }
-        });
-        mlPlayerUrl = Transformations.map(mlPlayer, new Function<List<Player>, String>() {
-            @Override
-            public String apply(List<Player> input) {
-                return application.getString(R.string.baseUrl2)
+
+        mlPlayerName = Transformations.map(
+                mlPlayer, input -> "이름: " + input.get(0).getName() );
+
+        mlBookmark = Transformations.map(
+                mlPlayer, input -> input.get(0).isBookmark() );
+
+        mlPosition = Transformations.map(
+                mlPlayer, input -> "포지션: " + input.get(0).getPosition() );
+
+        mlHeight = Transformations.map(
+                mlPlayer, input -> "신장: " + input.get(0).getHeight() );
+
+        mlFoot = Transformations.map(
+                mlPlayer, input -> "주발: " + input.get(0).getFoot() );
+
+        mlAge = Transformations.map(
+                mlPlayer, input -> "나이: " + input.get(0).getAge() + "세" );
+
+        mlShirt = Transformations.map(
+                mlPlayer, input -> "등번호: " + input.get(0).getShirt() );
+
+        mlPlayerUrl = Transformations.map(
+                mlPlayer,
+                input -> application.getString(R.string.baseUrl2)
                         + input.get(0).getId()
-                        + application.getString(R.string.png);
-            }
-        });
+                        + application.getString(R.string.png) );
     }
-
-
+    
 }
